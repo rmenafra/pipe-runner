3d10cd80bc9c1175259fa7d5f69a2f59babefde3
Guy Allard <w.g.allard@lumc.nl>
2017-12-06 09:46:30 +0100

    increased latency wait

--- pipe_runner/command_line.py
+++ pipe_runner/command_line.py
@@ -38,7 +38,7 @@ def runner(snakefile, directory, extraconfig, configfile, clusterconfig, additio
         args += ["--config"] + list(extraconfig)
 
     args += [
-        "--latency-wait", 120,
+        "--latency-wait", 180,
         "--drmaa", " -N preprocessor -pe BWA {cluster.threads} -l h_vmem={cluster.vmem} -q all.q -cwd -V -j Y",
         "--drmaa-log-dir", os.path.join(directory, "cluster_logs"),
         "--jobs", 100,
